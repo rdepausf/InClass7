81b3151731d6fda65280acffdeec82b67e58cf7b
rdepausf <rdepa@mail.usf.edu>
2020-03-10 21:51:04 -0400

    InClass 7 Changes

---
+++ UniversityOfSouthFlorida/DAL/SchoolContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UniversityOfSouthFlorida.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+
+namespace UniversityOfSouthFlorida.DAL
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
